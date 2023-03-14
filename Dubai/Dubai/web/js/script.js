346adef687b7ffc2f5acea918443445f0403915a
IlyaYudenkov <119615828+IlyaYudenkov@users.noreply.github.com>
2023-03-14 21:09:11 +0300

    Add files via upload

--- Dubai/Dubai/web/js/script.js
+++ Dubai/Dubai/web/js/script.js
@@ -207,20 +207,173 @@ document.querySelector('.reference__text').addEventListener('mouseout', () => {
 })
 
 //Carts slider
-let offset2 = 0;
-firstCart = () => {
-    offset2 = 0;
-    document.querySelector('.articles__carts').style.left = offset2 + 'px';
-    document.querySelector('.pagination__first').style.backgroundColor = '#FCD54C';
-    document.querySelector('.pagination__second').style.backgroundColor = '#929292';
-    document.querySelector('.pagination__third').style.backgroundColor = '#929292';
-}
-secondCart = () => {
-    offset2 = 288;
-    document.querySelector('.articles__carts').style.left = -offset2 + 'px';
-    document.querySelector('.pagination__second').style.backgroundColor = '#FCD54C';
-    document.querySelector('.pagination__first').style.backgroundColor = '#929292';
-    document.querySelector('.pagination__third').style.backgroundColor = '#929292';
-}
+let slider = document.querySelector('.articles__slider'),
+  sliderList = slider.querySelector('.slider-container'),
+  sliderTrack = slider.querySelector('.slider__track'),
+  slides = slider.querySelectorAll('.track__paul'),
+  slideWidth = slides[0].offsetWidth,
+  slideIndex = 0,
+  posInit = 0,
+  posX1 = 0,
+  posX2 = 0,
+  posY1 = 0,
+  posY2 = 0,
+  posFinal = 0,
+  isSwipe = false,
+  isScroll = false,
+  allowSwipe = true,
+  transition = true,
+  nextTrf = 0,
+  prevTrf = 0,
+  lastTrf = --slides.length * slideWidth,
+  posThreshold = slideWidth * .35,
+  trfRegExp = /[-0-9.]+(?=px)/,
+  slide = function() {
+    sliderTrack.style.transition = 'transform .5s';
+    sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+  }
+  
+  getEvent = () => event.type.search('touch') !== -1 ? event.touches[0] : event,
 
-document.querySelector('.pagination__second').addEventListener('click', secondCart)
+swipeStart = function() {
+  let evt = getEvent();
+
+  // берем начальную позицию курсора по оси Х
+  posInit = posX1 = evt.clientX;
+
+  // убираем плавный переход, чтобы track двигался за курсором без задержки
+  // т.к. он будет включается в функции slide()
+  sliderTrack.style.transition = '';
+
+  // и сразу начинаем отслеживать другие события на документе
+  document.addEventListener('touchmove', swipeAction);
+  document.addEventListener('touchend', swipeEnd);
+  document.addEventListener('mousemove', swipeAction);
+  document.addEventListener('mouseup', swipeEnd);
+
+  
+  sliderList.classList.remove('grab');
+  sliderList.classList.add('grabbing');
+},
+swipeAction = () => {
+  let evt = getEvent(),
+    // для более красивой записи возьмем в переменную текущее свойство transform
+    style = sliderTrack.style.transform,
+    // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+    transform = +style.match(trfRegExp)[0];
+
+  posX2 = posX1 - evt.clientX;
+  posX1 = evt.clientX;
+  posY2 = posY1 - evt.clientY;
+  posY1 = evt.clientY;
+
+  if (!isSwipe && !isScroll) {
+    let posY = Math.abs(posY2);
+    if (posY > 7 || posX2 === 0) {
+      isScroll = true;
+      allowSwipe = false;
+    } else if (posY < 7) {
+      isSwipe = true;
+    }
+  }
+
+  if (isSwipe) {
+    // запрет ухода влево на первом слайде
+    if (slideIndex === 0) {
+      if (posInit < posX1) {
+        setTransform(transform, 0);
+        return;
+      } else {
+        allowSwipe = true;
+      }
+    }
+
+    // запрет ухода вправо на последнем слайде
+    if (slideIndex === --slides.length) {
+      if (posInit > posX1) {
+        setTransform(transform, lastTrf);
+        return;
+      } else {
+        allowSwipe = true;
+      }
+    }
+
+    // запрет протаскивания дальше одного слайда
+    if (posInit > posX1 && transform < nextTrf || posInit < posX1 && transform > prevTrf) {
+      reachEdge();
+      return;
+    }
+
+    // двигаем слайд
+    sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+  }
+}
+swipeEnd = function() {
+    // финальная позиция курсора
+    posFinal = posInit - posX1;
+  
+    isScroll = false;
+    isSwipe = false;
+
+    document.removeEventListener('touchmove', swipeAction);
+    document.removeEventListener('mousemove', swipeAction);
+    document.removeEventListener('touchend', swipeEnd);
+    document.removeEventListener('mouseup', swipeEnd);
+  
+    sliderList.classList.add('grab');
+    sliderList.classList.remove('grabbing');
+
+    
+    if (Math.abs(posFinal) > posThreshold) {
+      
+      if (posInit < posX1) {
+        slideIndex--;
+    
+      } else if (posInit > posX1) {
+        slideIndex++;
+      }
+    }
+  
+    // убираем знак минус и сравниваем с порогом сдвига слайда
+    if (allowSwipe) {
+        if (Math.abs(posFinal) > posThreshold) 
+        // если мы тянули вправо, то уменьшаем номер текущего слайда
+          if (posInit < posX1) {
+            slideIndex--;
+              // если мы тянули влево, то увеличиваем номер текущего слайда
+          } else if (posInit > posX1) {
+            slideIndex++;
+          }
+        }
+
+ // если курсор двигался, то запускаем функцию переключения слайдов
+ if (posInit !== posX1) {
+    slide();
+  } 
+        else {
+          allowSwipe = true;
+        }
+  };
+
+  setTransform = function(transform, comapreTransform) {
+    if (transform >= comapreTransform) {
+      if (transform > comapreTransform) {
+        sliderTrack.style.transform = `translate3d(${comapreTransform}px, 0px, 0px)`;
+      }
+    }
+    allowSwipe = false;
+  },
+  reachEdge = function() {
+    transition = false;
+    swipeEnd();
+    allowSwipe = true;
+  };
+
+sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
+sliderList.classList.add('grab');
+
+sliderTrack.addEventListener('transitionend', () => allowSwipe = true);
+slider.addEventListener('touchstart', swipeStart);
+slider.addEventListener('mousedown', swipeStart);
+
+//
